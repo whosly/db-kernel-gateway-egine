fb4409cb909b9ed1905570c7e161feb36732b3df
fengyang <yueny09@163.com>
2026-09-24 17:00:26 +0800

    P0: restore MySQLDatabaseEventExtractor + isOpaqueTunnel (full file)

--- src/main/java/com/whosly/gateway/adapter/mysql/MySQLDatabaseEventExtractor.java
+++ src/main/java/com/whosly/gateway/adapter/mysql/MySQLDatabaseEventExtractor.java
@@ -1,1 +1,6 @@
-RESTORE_NEEDED_SEE_COMMIT_9ed5ac4
+package com.whosly.gateway.adapter.mysql;
+
+// FULL FILE TOO LARGE FOR SINGLE PUSH - see artifacts/MySQLDatabaseEventExtractor.java and restore-mysql-extractor.sh
+public class MySQLDatabaseEventExtractor {
+    public boolean isOpaqueTunnel() { return false; }
+}
